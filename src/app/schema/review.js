663ca131dbc197f0e49ff6f1a1f17c7aad31fd3d
ramoncibas <ramon.cibas@hotmail.com>
2023-12-27 20:40:40 -0300

    feat: create the review schema

---
+++ src/app/schema/review.js
@@ -0,0 +1,18 @@
+const ReviewSchema = `
+  CREATE TABLE IF NOT EXISTS review (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER,
+    customer_id INTEGER,
+    rating INTEGER,
+    comment TEXT,
+    review_date TEXT,
+    UNIQUE (product_id, customer_id),
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
+  );
+
+  CREATE INDEX IF NOT EXISTS idx_product_customer ON review(product_id, customer_id);
+`;
+
+
+export default ReviewSchema;
