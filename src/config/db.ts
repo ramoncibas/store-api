f1d1ca25f1bc0bf14ddb272357a7b58592330c6b
ramoncibas <ramon.cibas@hotmail.com>
2023-12-28 19:36:37 -0300

    fix: database connection file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,69 @@
-let sqlite3 = require("sqlite3").verbose();
-let db = new sqlite3.Database(process.env.DATABASE_URL, (err) => {
-  if (err) console.error('Error connecting to database:', err.message);
-});
-const schema = require('./schema');
+import sqlite3, { Database, RunResult } from "sqlite3";
 
-//creating the database
-function execute(db) {
-  return (
-    db.serialize(() => db.exec(schema))
-  );
-};
+class DatabaseManager {
+  private db: Database;
+  private databaseUrl: string = process.env.DATABASE_URL || "default";
 
-export default db.on("open", () => execute(db));
+  constructor() {
+    this.db = new sqlite3.Database(this.databaseUrl, (err) => {
+      if (err) {
+        console.error('Error connecting to database:', err.message);
+      }
+    });
+  }
+
+  public run(sql: string, params: any[]): Promise<RunResult> {
+    return new Promise<RunResult>((resolve, reject) => {
+      this.db.run(sql, params, function (err) {
+        if (err) {
+          console.error('Error executing query:', err.message);
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
+    });
+  }
+
+  public get(sql: string, params: any[]): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.db.get(sql, params, function (err, row) {
+        if (err) {
+          console.error('Error executing query:', err.message);
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+
+  public all(sql: string, params: any[]): Promise<any[]> {
+    return new Promise<any[]>((resolve, reject) => {
+      this.db.all(sql, params, function (err, rows) {
+        if (err) {
+          console.error('Error executing query:', err.message);
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  public close(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.db.close((err) => {
+        if (err) {
+          console.error('Error closing database connection:', err.message);
+          reject(err);
+        } else {
+          console.log('Database connection closed');
+          resolve();
+        }
+      });
+    });
+  }
+}
+
+export default DatabaseManager;
