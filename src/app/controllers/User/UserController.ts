9b3494b9738192765fb02dbfe10f3a517beeccff
ramoncibas <ramon.cibas@hotmail.com>
2024-01-03 18:32:18 -0300

    fix: refactor createUser for use in user registration

--- src/app/controllers/User/UserController.ts
+++ src/app/controllers/User/UserController.ts
@@ -1,15 +1,22 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import UserRepository from 'repositories/UserRepository';
 import UserFile from './UserFile';
-import { MulterRequest } from 'types/User.type';
 import UserError from 'errors/UserError';
+import { CustomRequest, User } from 'types/User.type';
+import { FileArray, UploadedFile } from 'express-fileupload';
+
+import { randomUUID } from 'crypto';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const { JWT_TOKEN_KEY, BUCKET_USER_PICTURE } = process.env;
 
 class UserController {
   static async getUser(req: Request, res: Response): Promise<void> {
     const { uuid } = req.params;
 
     try {
-      const user = await UserRepository.get(uuid);
+      const user = await UserRepository.getByPattern('uuid', uuid);
 
       if (!user) {
         const notFoundError = new UserError('User not found!', undefined, 404);
@@ -46,17 +53,59 @@ class UserController {
   }
 
   static async createUser(req: Request, res: Response): Promise<void> {
-    const fields = req.body;
-
-    if (Object.values(fields).some(value => typeof value !== 'string' || value.trim() === '')) {
-      const invalidDataError = new UserError('All fields must be filled!', undefined, 400);
-      res.status(400).send(invalidDataError.toResponseObject());
-      return;
-    }
-
     try {
-      await UserRepository.create(fields);
-      res.status(201).redirect("/profile");
+      const {
+        first_name,
+        last_name,
+        email,
+        password,
+      }: User = req.body;
+
+      const files = req.files as { user_picture?: FileArray | UploadedFile } || null;
+
+      if (Object.values(req.body).some(value => typeof value !== 'string' || value.trim() === '')) {
+        const invalidDataError = new UserError('All fields must be filled!', undefined, 400);
+        res.status(400).send(invalidDataError.toResponseObject());
+        return;
+      }
+
+      const existingUser = await UserRepository.getByPattern('email', email);
+
+      if (existingUser) {
+        res.status(409).send("User Already Exists. Please Login");
+        return;
+      }
+
+      const userUUID = randomUUID();
+
+      if (files && files.user_picture) {
+        await UserFile.saveUserPicture(userUUID, files.user_picture);
+      }
+
+      const encryptedPassword = await bcrypt.hash(password, 10);
+
+      const user_picture_name: string | null = files?.user_picture
+        ? `${userUUID}_${files.user_picture.name}`
+        : null;
+
+      const user = await UserRepository.create({
+        uuid: userUUID,
+        first_name,
+        last_name,
+        email: email.toLowerCase(),
+        password: encryptedPassword,
+        user_picture_name: user_picture_name
+      });
+
+      const token = jwt.sign(
+        { user_id: user.id, email },
+        JWT_TOKEN_KEY!,
+        { expiresIn: "1h" }
+      );
+
+      user.token = token;
+      console.log(user);
+      res.status(201).json(user);
 
     } catch (error: any) {
       const createUserError = new UserError('Error creating user', error);
@@ -66,9 +115,11 @@ class UserController {
     }
   }
 
-  static async updateUser(req: MulterRequest, res: Response): Promise<void> {
+  static async updateUser(req: CustomRequest, res: Response, next: NextFunction): Promise<void> {
     const { phone, email, user_picture_name } = req.body;
-    const userPictureFile = req.files.find(file => file.fieldname === 'user_picture');
+    const userPictureFile = Array.isArray(req.files)
+      ? req.files.find((file) => file.name === 'user_picture')
+      : undefined;
 
     try {
       const userUUID = req.params.uuid;
