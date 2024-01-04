8307f116584331c414138e1e11bcb794f3883ae3
ramoncibas <ramon.cibas@hotmail.com>
2024-01-04 20:36:23 -0300

    fix: adjusts the middleware (isadmin) to use the user repository

--- src/app/middlewares/isAdmin.ts
+++ src/app/middlewares/isAdmin.ts
@@ -1,26 +1,27 @@
 import { Request, Response, NextFunction } from "express";
-import findUserBy from "../models/User/findUserBy";
-import { User } from "../types/User.type";
+import UserRepository from "repositories/UserRepository";
+import UserError from "errors/UserError";
+import { User } from "types/User.type";
 
 const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
-  const uuid = req.params.uuid 
-  const messageWrongUser = "Ops! Something is wrong with your data. You are not able to acces this page";
+  const uuid: string = req.params.uuid 
+  const messageWrongUser: string = "Ops! Something is wrong with your data. You are not able to acces this page";
 
   if (!uuid) {
     return res.status(403).send(messageWrongUser);
   }
   
   try {
-    const findUser = new findUserBy();
-    const [user]: any = await findUser.uuid(uuid);
-    console.log('user:', user)
-    if(user.type != 'admin') {
+    const user: User | null = await UserRepository.getByPattern('uuid', uuid);
+
+    if(user && user?.type != 'admin') {
       return res.status(403).send(messageWrongUser);
     }
 
   } catch (error) {
     console.log(error)
-    return res.status(401).send("Invalid User Data");
+    res.status(401).send(messageWrongUser);
+    throw new UserError(messageWrongUser, error, 401);
   }
   return next();
 };
