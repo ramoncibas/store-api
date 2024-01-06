a5d07350b5443a978bb01f30a79fe6fb48141b7e
ramoncibas <ramon.cibas@hotmail.com>
2024-01-06 16:15:40 -0300

    fix: customer reoute names

--- src/app/routes/Customer/Customer.route.ts
+++ src/app/routes/Customer/Customer.route.ts
@@ -5,9 +5,9 @@ import CustomerController from 'controllers/Customer/CustomerController';
 
 const router = Router();
 
-router.get('/customer/:uuid', authMiddleware, CustomerController.getCustomer);
-router.post('/customer/create', authMiddleware, CustomerController.createCustomer);
-router.post('/customer/:uuid/reviews', authMiddleware, CustomerController.createReview);
-router.patch("/customer/:uuid", authMiddleware, CustomerController.updateCustomer);
+router.get('/:uuid', authMiddleware, CustomerController.getCustomer);
+router.post('/create', authMiddleware, CustomerController.createCustomer);
+router.post('/:uuid/reviews', authMiddleware, CustomerController.createReview);
+router.patch("/:uuid", authMiddleware, CustomerController.updateCustomer);
 
 export default router;
