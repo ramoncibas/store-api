3243eb89bb5794f2598d251b62fd40dd41d1ed1f
ramoncibas <ramon.cibas@hotmail.com>
2024-01-08 22:32:41 -0300

    fix: return path of file or null (function doesFileExist)

--- src/utils/doesFileExist.ts
+++ src/utils/doesFileExist.ts
@@ -6,17 +6,17 @@ import fs from "fs";
  * @param {string} prefix - The prefix to look for in the file names.
  * @returns {Promise<string | null>} - A Promise that resolves to the name of the first file found with the given prefix, or null if no such file exists.
  */
-async function doesFileExist(prefix: string, path: string) {
+async function doesFileExist(prefix: string, path: string): Promise<string | null> {
 
   try {
     const files = await fs.promises.readdir(path);
 
     const exists = files.find((file: any) => file.startsWith(`${prefix}_`));
    
-    return exists;
+    return exists ?? null;
   } catch (error) {
     console.error('Error reading directory:', error);
-    return false;
+    return null;
   }
 }
 
