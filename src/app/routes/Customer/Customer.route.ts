c4654b12acce5a5cce3c1d4a9bacb7f2822b8cc5
ramoncibas <ramon.cibas@hotmail.com>
2024-01-10 16:16:47 -0300

    fix: review route

--- src/app/routes/Customer/Customer.route.ts
+++ src/app/routes/Customer/Customer.route.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get('/:uuid', authMiddleware, CustomerController.getCustomer);
 router.post('/create', authMiddleware, CustomerController.createCustomer);
-router.post('/:uuid/reviews', authMiddleware, CustomerController.createReview);
+router.post('/:uuid/review', authMiddleware, CustomerController.createReview);
 router.patch("/:uuid", authMiddleware, CustomerController.updateCustomer);
 
 export default router;
