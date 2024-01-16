a138a574d2e7f0255fa6a3f42c046e7b3b724469
ramoncibas <ramon.cibas@hotmail.com>
2024-01-15 21:15:31 -0300

    feat: add seeds

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
-dotenv.config();
+import config from 'config/environment';
+dotenv.config(config.envFilePath);
 
 import express, { Application } from 'express';
 import cors from 'cors';
@@ -11,8 +12,10 @@ import { errorHandler } from 'middlewares';
 import swaggerUi from 'swagger-ui-express';
 import swaggerOutput from './swagger.json';
 import cleanupTempFiles from 'utils/cleanupTempFiles';
+import Seeder from 'lib/seeds';
 
 const app: Application = express();
+const seeds = new Seeder();
 
 const { PORT = 5000 } = process.env;
 app.use(cors());
@@ -26,6 +29,11 @@ app.use(appRoutes);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
 app.listen(PORT, () => {
-  cleanupTempFiles();
   console.log(`Server is running - Port: ${PORT}...`);
+  cleanupTempFiles();
+
+  if (process.env.NODE_ENV === 'development') {
+    seeds.createDatabase();
+    seeds.seedDatabase(); 
+  }
 });
