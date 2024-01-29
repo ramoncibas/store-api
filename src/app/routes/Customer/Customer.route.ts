f8de70c5f3a3362fe361ef20e78e4b34a0b839d5
ramoncibas <ramon.cibas@hotmail.com>
2024-01-29 00:49:54 -0300

    fix: remove review route

--- src/app/routes/Customer/Customer.route.ts
+++ src/app/routes/Customer/Customer.route.ts
@@ -7,7 +7,6 @@ const router = Router();
 
 router.get('/:uuid', authMiddleware, CustomerController.getCustomer);
 router.post('/create', authMiddleware, CustomerController.createCustomer);
-router.post('/:uuid/review', authMiddleware, CustomerController.createReview);
 router.patch("/:uuid", authMiddleware, CustomerController.updateCustomer);
 router.delete('/delete/:uuid', authMiddleware, CustomerController.deleteCustomer);
 
