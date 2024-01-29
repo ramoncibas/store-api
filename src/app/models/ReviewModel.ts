7c53953611db61197f77a822698423d41fea1b81
ramoncibas <ramon.cibas@hotmail.com>
2024-01-29 00:47:31 -0300

    fix: review model

--- src/app/models/ReviewModel.ts
+++ src/app/models/ReviewModel.ts
@@ -1,6 +1,7 @@
 import DatabaseManager from "../../database/db";
 import Review from "types/Review.type";
 import { randomUUID } from 'crypto';
+import { RunResult } from "sqlite3";
 
 class ReviewModel {
   private static dbManager: DatabaseManager;
@@ -17,22 +18,33 @@ class ReviewModel {
    * @param review - Object representing the review data to be saved.
    * @returns A Promise that resolves when the operation is completed.
    */
-  static async save(review: Review): Promise<void> {
+  static async save(review: Review): Promise<Review> {
     const query: string = `
       INSERT INTO review (
         uuid,
         product_id,
         customer_id,
         rating,
-        comment,
-        review_date
-      ) VALUES (?, ?, ?, ?, ?, ?)
+        comment
+      ) VALUES (?, ?, ?, ?, ?)
+      RETURNING *;
     `;
 
     try {
+      const generatedUuid = randomUUID();
+
+      const values = [
+        generatedUuid,
+        review.product_id,
+        review.customer_id,
+        review.rating,
+        review.comment,
+      ];
+
       const dbManager = this.getDBManager();
-      const reviewUUID = randomUUID();
-      await dbManager.run(query, [reviewUUID, ...Object.values(review)]);
+      const reviewData = await dbManager.all(query, values);
+
+      return reviewData[0];
     } catch (error) {
       console.error(error);
       throw error;
@@ -60,13 +72,38 @@ class ReviewModel {
     }
   }
 
+   /**
+   * Get a review from the database based on the provided pattern and value.
+   * @param pattern - A string or array of strings representing the fields to filter on.
+   * @param values - A string or array of strings corresponding values for the filter pattern.
+   * @returns A Promise that resolves with the review data or null if not found.
+   */
+   static async getByPattern(pattern: string | Array<string>, values: number | string | Array<string>): Promise<Review[] | null> {
+    const conditions = Array.isArray(pattern) ? pattern.join(' AND ') : pattern;
+    const placeholders = Array.isArray(values) ? values.map(() => '?').join(', ') : '?';
+    const queryValues = Array.isArray(values) ? values : [values];
+    
+    const query: string = `
+      SELECT * FROM review WHERE ${conditions} = ${placeholders}
+    `;
+
+    try {
+      const dbManager = this.getDBManager();
+      const row: Review[] = await dbManager.all(query, queryValues);
+      return row;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
   /**
    * Update the data of a review in the database.
    * @param reviewUUID - UUID of the review to be updated.
    * @param updatedFields - Object containing the fields to be updated.
    * @returns A Promise that resolves when the operation is completed.
    */
-  static async update(reviewUUID: string, updatedFields: Partial<Review>): Promise<void> {
+  static async update(reviewUUID: string, updatedFields: Partial<Review>): Promise<Review> {
     const keys = Object.keys(updatedFields);
     const values = Object.values(updatedFields);
 
@@ -76,11 +113,14 @@ class ReviewModel {
       UPDATE review
       SET ${setClause}
       WHERE uuid = ?
+      RETURNING *;
     `;
 
     try {
       const dbManager = this.getDBManager();
-      await dbManager.run(query, [...values, reviewUUID]);
+      const reviewData = await dbManager.all(query, [...values, reviewUUID]);
+
+      return reviewData[0];   
     } catch (error) {
       console.error(error);
       throw error;
@@ -92,14 +132,14 @@ class ReviewModel {
    * @param reviewUUID - UUID of the review to be deleted.
    * @returns A Promise that resolves when the operation is completed.
    */
-  static async delete(reviewUUID: string): Promise<void> {
+  static async delete(reviewUUID: string): Promise<RunResult> {
     const query: string = `
       DELETE FROM review WHERE uuid = ?
     `;
 
     try {
       const dbManager = this.getDBManager();
-      await dbManager.run(query, [reviewUUID]);
+      return await dbManager.run(query, [reviewUUID]);
     } catch (error) {
       console.error(error);
       throw error;
